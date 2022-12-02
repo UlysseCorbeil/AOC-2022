f78f9ed531adfb4190290865469e9fa0af3047e9
Ulysse <ulysse.corbeil@isios.ca>
2022-12-02 09:51:35 -0500

    cleaner solution to p2

--- Day 2/2.py
+++ Day 2/2.py
@@ -6,43 +6,41 @@ file = open(local_file,  "r")
 
 data = file.read()
 list_moves = []
-for k in data.splitlines():
-    list_moves.append(k.split(" "))
 
 rnd_points = 0
-for moves in list_moves:
-    first = moves[0]
-    last = moves[1]
 
-    if first == "A":
-        if last == "X":
-            rnd_points += 3
-            rnd_points += 0
-        elif last == "Y":
-            rnd_points += 1
-            rnd_points += 3
-        elif last == "Z":
-            rnd_points += 2
-            rnd_points += 6
-    if first == "B":
-        if last == "X":
-            rnd_points += 1
-            rnd_points += 0
-        elif last == "Y":
-            rnd_points += 2
-            rnd_points += 3
-        elif last == "Z":
-            rnd_points += 3
-            rnd_points += 6
-    if first == "C":
-        if last == "X":
-            rnd_points += 2
-            rnd_points += 0
-        elif last == "Y":
-            rnd_points += 3
-            rnd_points += 3
-        elif last == "Z":
-            rnd_points += 1
-            rnd_points += 6
+p_1 = {
+    "A X": 1 + 3,
+    "A Y": 2 + 6,
+    "A Z": 3 + 0,
+
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+
+    "C X": 1 + 6,
+    "C Y": 2 + 0,
+    "C Z": 3 + 3,
+}
+
+p_2 = {
+    "A X": 3 + 0,
+    "A Y": 1 + 3,
+    "A Z": 2 + 6,
+
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+
+    "C X": 2 + 0,
+    "C Y": 3 + 3,
+    "C Z": 1 + 6,
+}
+
+for k in data.splitlines():
+    # rnd_points += p_1[k]
+    
+    rnd_points += p_2[k]
+
 
 print(rnd_points)
